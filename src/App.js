a83222d5871b882017be4a20a2dbdf66b3809ade
mariegrace31 <mariegracebmg@gmail.com>
2023-08-16 12:58:30 +0200

    Update the app.js file and install store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import DetailsPage from './pages/DetailsPage';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/details/:name" element={<DetailsPage />} />
+      </Routes>
     </>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchEmojiAsync } from '../redux/emojis/emojisSlice';
 
 const HomePage = () => (
   <div>
