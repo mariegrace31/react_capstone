372d5114210ca8df1324122476c4c8163fb67936
mariegrace31 <mariegracebmg@gmail.com>
2023-08-18 14:51:22 +0200

    Add routes

---
+++ src/routes/Details.jsx
@@ -0,0 +1,11 @@
+import DetailsPage from 'components/DetailsPage';
+import Footer from 'components/Footer';
+
+const Details = () => (
+  <>
+    <DetailsPage />
+    <Footer />
+  </>
+);
+
+export default Details;
---
+++ src/routes/NotMatch.jsx
@@ -0,0 +1,7 @@
+const NotMatch = () => (
+  <div>
+    <p>Sorry! Page not found</p>
+  </div>
+);
+
+export default NotMatch;
