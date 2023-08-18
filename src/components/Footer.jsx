14980cd5b756940deb5840533581af35d681def0
mariegrace31 <mariegracebmg@gmail.com>
2023-08-18 14:29:56 +0200

    Add the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <div className="footer">
+    <p>Copyright© 2023</p>
+    <p className="email">mariegracebmg@gmail.com</p>
+    <p>Some rights reserved</p>
+  </div>
+);
+
+export default Footer;
